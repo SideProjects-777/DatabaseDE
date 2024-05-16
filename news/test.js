b3748d36a2cb3a9c4c65f92d895d537dd2daf8d4
Sattar Rahimbayli <sattar.rahimbayli@dedalus.eu>
2024-05-16 14:59:06 +0200

    Cleaning up for master branch

--- news/test.js
+++
@@ -1,9 +0,0 @@
-const currentDate = new Date();
-const currentDay = currentDate.getDate();
-const currentMonth = currentDate.getMonth();
-const currentYear = currentDate.getFullYear();
-
-const startOfDay = new Date(currentYear, currentMonth, currentDay, 0, 0, 0, 0);
-const millisecondsSinceEpoch = startOfDay.getTime();
-
-console.log(millisecondsSinceEpoch);
